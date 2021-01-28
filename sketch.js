ca1e60fd2b38af3653e7616c876debedd9d61656
TheBurger21 <73203647+TheBurger21@users.noreply.github.com>
2021-01-27 19:27:19 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+
+var car,wall;
+
+
+var speed, weight; 
+
+
+function setup() {
+  createCanvas(1600, 400);
+
+
+	speed=random(55,90)
+	weight=random(400,1500)
+
+
+	bullet=createSprite(50, 200, 40, 20);   
+
+	bullet.velocityX = speed;
+
+	bullet.shapeColor=color(255);
+
+
+  
+  	wall=createSprite(1500,200, 60, height/2)
+  	wall.shapeColor=color(80,80,80)
+}
+
+
+function draw() {
+  background(0);
+
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
+  {
+  	bullet.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22500;
+	if(deformation>180)
+	{
+		wall.shapeColor=color(255,0,0);
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		wall.shapeColor=color(230,230,0);
+	}
+
+	if(deformation<100)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+  }  
+  
+  drawSprites();
+ 
+}
+
+
